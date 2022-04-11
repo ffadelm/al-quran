cdd1a552d2812170959453d704973b05db12a17c
ffadelm <faysalfadelm0107@gmail.com>
2022-04-11 09:18:15 +0700

    responsive proggress 📝

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,72 @@
 <template>
-  <router-view />
+  <div class="container">
+    <router-view />
+  </div>
 </template>
 
-<style>
+<style scoped>
+.container {
+  width: 92%;
+  margin: 0 auto 30px auto;
+}
+@media screen and (min-width: 600px) {
+  .container {
+    width: 90%;
+  }
+}
+@media screen and (min-width: 770px) {
+  .container {
+    width: 86%;
+  }
+}
+@media screen and (min-width: 900px) {
+  .container {
+    width: 80%;
+  }
+}
+@media screen and (min-width: 980px) {
+  .container {
+    width: 77%;
+  }
+}
+@media screen and (min-width: 1060px) {
+  .container {
+    width: 73%;
+  }
+}
+@media screen and (min-width: 1100px) {
+  .container {
+    width: 69%;
+  }
+}
+@media screen and (min-width: 1160px) {
+  .container {
+    width: 63%;
+  }
+}
+@media screen and (min-width: 1300px) {
+  .container {
+    width: 60%;
+  }
+}
+@media screen and (min-width: 1450px) {
+  .container {
+    width: 57%;
+  }
+}
+@media screen and (min-width: 1570px) {
+  .container {
+    width: 54%;
+  }
+}
+@media screen and (min-width: 1750px) {
+  .container {
+    width: 49%;
+  }
+}
+@media screen and (min-width: 1850px) {
+  .container {
+    width: 46%;
+  }
+}
 </style>
