f2e75ab55adfaedde9cd2dd88be3daf3434c2a70
ffadelm <faysalfadelm0107@gmail.com>
2022-04-12 13:12:10 +0700

    modified styiling 👌

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
-  <router-link to="/">Al-Qur'an</router-link>
+  <router-link to="/">Al - Qur'an</router-link>
+
   <div class="languages">
-    <span @click="toIndonesia">Indonesia</span>
-    <span @click="toEnglish">English</span>
+    <span @click="toIndonesia">
+      <img src="https://img.icons8.com/office/16/000000/indonesia--v1.png" />
+      Indonesia
+    </span>
+    <span @click="toEnglish">
+      <img src="https://img.icons8.com/office/16/000000/great-britain.png" />
+      English
+    </span>
   </div>
 </template>
 
@@ -12,14 +19,17 @@ import { useStore } from "vuex";
 export default {
   setup() {
     const store = useStore();
+
     function toIndonesia() {
       store.commit("changeLanguage", "id");
       localStorage.setItem("language", "id");
     }
+
     function toEnglish() {
       store.commit("changeLanguage", "eng");
       localStorage.setItem("language", "eng");
     }
+
     return {
       toIndonesia,
       toEnglish,
@@ -36,20 +46,24 @@ a {
   font-weight: bold;
   font-size: 1.9rem;
   text-align: right;
+  font-style: italic;
   letter-spacing: 5px;
-  font-family: poppins;
+  font-family: cursive;
   text-decoration: none;
   transition: color 0.1s;
 }
+
 a:hover {
   color: #066163;
 }
+
 .languages {
   color: #383838;
   margin-top: -2px;
   text-align: right;
   margin-bottom: 28px;
 }
+
 span {
   font-size: 1em;
   cursor: pointer;
@@ -58,10 +72,12 @@ span {
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
+
 span:hover {
   text-decoration: underline;
   color: #cdbe78;
 }
+
 @media screen and (min-width: 1160px) {
   span {
     font-size: 0.9rem;
