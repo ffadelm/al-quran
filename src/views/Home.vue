c72e4ea7d65806c864851e8c6c2116469661a6be
ffadelm <faysalfadelm0107@gmail.com>
2022-04-13 10:46:13 +0700

    modified Home View 👁️‍🗨️

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,35 @@
 <template>
   <Header />
+
+  <div v-if="isLoading">
+    <Skeleton />
+  </div>
+  <div v-else v-for="surah in data" :key="surah.number_of_surah">
+    <Card :surah="surah" />
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import useFetch from "@/use/useFetch.js";
 import Header from "@/components/Header.vue";
+import Card from "@/components/Cards.vue";
+import Skeleton from "@/components/Skeletons.vue";
 
 export default {
   name: "Home",
   components: {
     Header,
+    Card,
+    Skeleton,
+  },
+  setup() {
+    const { data, isLoading } = useFetch(
+      "https://raw.githubusercontent.com/penggguna/QuranJSON/master/quran.json"
+    );
+    return {
+      data,
+      isLoading,
+    };
   },
 };
 </script>
