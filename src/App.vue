2d5cbd8c4fe15f7b39602ce574f38d80a3104a04
ffadelm <faysalfadelm0107@gmail.com>
2022-04-13 10:44:20 +0700

    scroll up 🔼

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,58 @@
 <template>
   <div class="container">
     <router-view />
+    <div v-if="scrollValue !== 0" @click="scrollToTop" class="scroll-top">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path d="M0 0h24v24H0z" fill="none" />
+        <path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z" />
+      </svg>
+    </div>
   </div>
 </template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const scrollValue = ref(window.scrollY);
+    function scrollToTop() {
+      window.scrollTo(0, 0);
+    }
+    window.addEventListener("scroll", () => {
+      scrollValue.value = window.scrollY;
+    });
+    return {
+      scrollToTop,
+      scrollValue,
+    };
+  },
+};
+</script>
 
 <style scoped>
 .container {
   width: 92%;
   margin: 0 auto 30px auto;
+}
+.scroll-top {
+  top: 90%;
+  right: 0;
+  padding: 8px;
+  cursor: pointer;
+  position: fixed;
+  font-size: 1.3rem;
+  border-radius: 4px;
+  margin-right: 28px;
+  transition: transform 0.1s;
+  background-color: #066163;
+}
+.scroll-top:hover {
+  transform: scale(1.1);
 }
 @media screen and (min-width: 600px) {
   .container {
