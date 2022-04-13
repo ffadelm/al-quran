b17a0086ab152d65325ccf27d1a9a7d197690600
ffadelm <faysalfadelm0107@gmail.com>
2022-04-13 10:43:49 +0700

    skeleton & ayat 📝

---
+++ src/components/Ayat.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="card">
+    <span class="number">{{ verse.number }}</span>
+    <p class="verse">{{ verse.text }}</p>
+    <p class="translation">
+      {{ language === "eng" ? verse.translation_en : verse.translation_id }}
+    </p>
+
+    <details v-if="language === 'id'">
+      <summary>Tafsir</summary>
+      <p class="interpretation">
+        {{ interpretation }}
+      </p>
+    </details>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+export default {
+  props: ["verse", "interpretation"],
+  setup() {
+    const language = computed(() => useStore().getters.getLanguage);
+    return { language };
+  },
+};
+</script>
+
+<style>
+.card {
+  color: #4a4949;
+  border-top: 5px solid #253a50;
+}
+.number {
+  padding: 3px;
+  font-size: 1.1rem;
+  font-weight: bold;
+  border-radius: 4px;
+  font-family: monospace;
+}
+@font-face {
+  font-family: quranFont;
+  src: url("/quran.OTF");
+}
+.verse {
+  margin-top: 25px;
+  font-size: 1.8rem;
+  text-align: right;
+  line-height: 50px;
+  margin-bottom: 35px;
+  font-family: quranFont;
+}
+.interpretation,
+.translation {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+summary,
+.translation,
+.interpretation {
+  font-size: 1rem;
+  line-height: 24px;
+  letter-spacing: 1px;
+}
+details {
+  margin-top: 30px;
+}
+summary {
+  outline: none;
+}
+summary:hover {
+  cursor: pointer;
+}
+.interpretation {
+  margin-top: 20px;
+}
+@media screen and (min-width: 760px) {
+  .verse {
+    line-height: 64px;
+  }
+  summary,
+  .translation,
+  .interpretation {
+    font-size: 1rem;
+    line-height: 31px;
+  }
+}
+</style>
---
+++ src/components/Skeletons.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="skeleton">
+    <div class="block first_block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+  </div>
+  <div class="skeleton">
+    <div class="block first_block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+  </div>
+  <div class="skeleton">
+    <div class="block first_block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+  </div>
+  <div class="skeleton">
+    <div class="block first_block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+  </div>
+  <div class="skeleton">
+    <div class="block first_block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+  </div>
+  <div class="skeleton">
+    <div class="block first_block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+    <div class="block"></div>
+  </div>
+</template>
+
+<style scoped>
+.skeleton {
+  width: 92%;
+  padding: 18px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border-top: 5px solid #066163;
+}
+
+.first_block {
+  margin-top: 22px;
+}
+
+.block {
+  width: 100%;
+  height: 26px;
+  border-radius: 4px;
+  margin-bottom: 25.4px;
+  animation: loading 1s ease-in-out infinite alternate;
+}
+
+@media screen and (min-width: 770px) {
+  .skeleton {
+    border-top: 5px solid #066163;
+  }
+}
+
+@keyframes loading {
+  0% {
+    transform: scale(1);
+    background-color: rgba(165, 165, 165, 0.1);
+  }
+
+  50% {
+    transform: scale(1.01);
+    background-color: rgba(165, 165, 165, 0.3);
+  }
+
+  100% {
+    transform: scale(1);
+    background-color: rgba(165, 165, 165, 0.1);
+  }
+}
+</style>
